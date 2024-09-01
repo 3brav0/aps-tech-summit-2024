1b2916acca00d60a852e98117c0c802aae3fcf1d
Elias Bravo <epkbravo@outlook.com>
2024-09-01 11:59:23 -0400

    updated router paths

--- src/App.tsx
+++ src/App.tsx
@@ -40,13 +40,13 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
+        <Route exact path="/aps-tech-summit-2024/home">
           <Home />
         </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
+        <Route exact path="/aps-tech-summit-2024/">
+          <Redirect to="/aps-tech-summit-2024/home" />
         </Route>
-        <Route exact path="/schedule">
+        <Route exact path="/aps-tech-summit-2024/schedule">
           <Schedule />
         </Route>
       </IonRouterOutlet>
