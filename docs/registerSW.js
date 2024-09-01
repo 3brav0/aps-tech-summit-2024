5680b483fbb670cf22d980f9d3911d8f75fa7010
Elias Bravo <epkbravo@outlook.com>
2024-09-01 11:08:22 -0400

    updated path for service worker

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/aps-tech-summit-2024/sw.js', { scope: '/' })})}
